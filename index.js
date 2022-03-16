f22432980980e6c5a801531121e487ad03fbbd13
Tomáš Müller <mullert@img.cas.cz>
2022-03-16 07:59:49 +0100

    improved import option

---
+++ index.js
@@ -0,0 +1,7 @@
+import DjangoForm from "./lib/DjangoForm";
+import DjangoFormSet from "./lib/DjangoFormSet";
+import {form, formsetValues} from "./lib/composables/forms";
+
+export {
+    DjangoForm, DjangoFormSet, form, formsetValues
+}
